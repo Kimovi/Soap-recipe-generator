744c04e5b334fa88ff345e02546a079e52f96267
Bora Kim <stellakkkk@hotmail.com>
2021-02-26 08:45:04 +0000

    Ports corrected for each application and docker-compose file added

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -8,9 +8,9 @@ from application import app, db
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def gen():
-    mainIngredient_response = requests.get("http://localhost:5001/mainIngredient")
-    oilIngredient_response = requests.get("http://localhost:5002/oilIngredient")
-    benefit_response = requests.post("http://localhost:5003/benefit", data = mainIngredient_response.text)
+    mainIngredient_response = requests.get("http://35.247.62.150:5001/mainIngredient")
+    oilIngredient_response = requests.get("http://35.247.62.150:5002/oilIngredient")
+    benefit_response = requests.post("http://35.247.62.150:5003/benefit", data = mainIngredient_response.text)
 
     new_build = Soap(mainIngredient = mainIngredient_response.text, oilIngredient = oilIngredient_response.text, benefit = benefit_response.text)
     db.session.add(new_build)
